fc9803f319af0dbc6a5e0e0e357709c7ef68cc0b
Xofmen <113815570+Xofmen@users.noreply.github.com>
2022-10-25 18:46:36 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let str = 'Akunamatata'
+console.log(str);
+let amega = str.slice(5, 8)
+console.error(amega);
+
+
+let alpha = 'Akunamatata'
+console.log(alpha);
+let beta = alpha.substring(1, 4)
+console.warn(beta);
+
+
+let txt = 'alex'.split('')
+console.log(txt);
+let txt_2 = txt.join('')
+console.log(txt_2);
+txt_2 = txt_2.replace(/a/, 'o')
+txt_2 = txt_2.replace(/e/, '')
+console.log(txt_2);
+
+
+
